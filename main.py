ba2989ed12198e77be14b58216e126d30f599383
stepkos <stepkos@wp.pl>
2022-01-16 21:12:23 +0100

    generate 10 random

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from src.spriteGenerator import SpriteGenerator
-
+import random
 # background face, hair    face_color, hair_color
-sprite = SpriteGenerator(1, (0, 1, 1), (2, 3, 2))
-sprite.img.show()
+
+
+for _ in range(10):
+    bg = random.randint(0, 3)
+
+    f = random.randint(0, 1)
+    h = random.randint(0, 4)
+    e = random.randint(0, 2)
+
+    fc = random.randint(0, 3)
+    hc = random.randint(0, 3)
+    ec = random.randint(0, 2)
+    sprite = SpriteGenerator(bg, (f, h, e), (fc, hc, ec))
+    sprite.img.save('out/{}{}{}{}{}{}{}.png'.format(bg, f, h, e, fc, hc, ec))
 
 
 
