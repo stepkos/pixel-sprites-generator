3d8c2298c307683a8cfd7a5c49f7c28db0ba690d
stepkos <stepkos@wp.pl>
2022-01-16 23:22:35 +0100

    main

--- main.py
+++ main.py
@@ -4,19 +4,24 @@ import random
 # c - color
 # SpriteGenerator(bg, (f, h, e), (fc, hc, ec))
 
+def generate_random(amount):
+    for _ in range(amount):
+        bg = random.randint(0, 3)
 
-for _ in range(10):
-    bg = random.randint(0, 3)
+        f = random.randint(0, 1)
+        h = random.randint(0, 6)
+        e = random.randint(0, 5)
 
-    f = random.randint(0, 1)
-    h = random.randint(0, 6)
-    e = random.randint(0, 5)
+        fc = random.randint(0, 3)
+        hc = random.randint(0, 3)
+        ec = random.randint(0, 2)
+        sprite = SpriteGenerator(bg, (f, h, e), (fc, hc, ec))
+        sprite.img.save('out/{}{}{}{}{}{}{}.png'.format(bg, f, h, e, fc, hc, ec))
 
-    fc = random.randint(0, 3)
-    hc = random.randint(0, 3)
-    ec = random.randint(0, 2)
-    sprite = SpriteGenerator(bg, (f, h, e), (fc, hc, ec))
-    sprite.img.save('out/{}{}{}{}{}{}{}.png'.format(bg, f, h, e, fc, hc, ec))
 
+# SpriteGenerator(3, (0, 5, 5), (0, 1, 1)).img.save('my_sprite.png')
+
+if __name__ == '__main__':
+    generate_random(10)
 
 
