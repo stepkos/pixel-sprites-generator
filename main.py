0a7606938e01fd3b8b2aa34e04586af6c65d55b8
stepkos <stepkos@wp.pl>
2022-01-16 20:43:37 +0100

    new templates

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from src.spriteGenerator import SpriteGenerator
 
-# face, hair    face_color, hair_color
-sprite = SpriteGenerator(templates=(0, 0), colors=(5, 3))
+# background face, hair    face_color, hair_color
+sprite = SpriteGenerator(1, (0, 1, 1), (2, 3, 2))
 sprite.img.show()
+
+
+
--- src/spriteGenerator.py
+++ src/spriteGenerator.py
@@ -1,3 +1,4 @@
+from ast import Str
 from typing import Iterable
 from PIL import Image, ImageColor
 from json import load as json_load
@@ -10,9 +11,14 @@ colors = json_load(open('colors.json'))
 
 
 class SpriteGenerator:
-    def __init__(self, templates: Iterable, colors: Iterable):
-        self.img = SpriteGenerator.mix_template(*templates)
+    def __init__(self, bg: int, templates: Iterable, colors: Iterable):
+        self.img = SpriteGenerator.mix_template(bg, *templates)
         self.img = SpriteGenerator.apply_colors(self.img, *colors)
+
+
+    @classmethod
+    def generate_by_id(id: str):
+        pass
 
 
     @staticmethod
@@ -21,26 +27,38 @@ class SpriteGenerator:
             for y in range(Y_SIZE):
                 if layer2.getpixel((x, y)) != (0, 0, 0, 0):
                     layer1.putpixel((x, y), layer2.getpixel((x, y)))
-                    
+
         return layer1
 
 
     @staticmethod
-    def mix_template(face, hair):
+    def mix_template(bg, face, hair, eyes):
+        img = Image.open(TEMPLATES_PATH + 'bgs/bg{}.png'.format(bg))
         face = Image.open(TEMPLATES_PATH + 'faces/face{}.png'.format(face))
         hair = Image.open(TEMPLATES_PATH + 'hair/hair{}.png'.format(hair))
-        return SpriteGenerator.override_layer(face, hair)
+        eyes = Image.open(TEMPLATES_PATH + 'eyes/eyes{}.png'.format(eyes))
+        img = SpriteGenerator.override_layer(img, face)
+        img = SpriteGenerator.override_layer(img, eyes)
+        img = SpriteGenerator.override_layer(img, hair)
+
+        return img
 
 
     @staticmethod
-    def apply_colors(img, face, hair):
+    def apply_colors(img, face, hair, eyes):
         for x in range(X_SIZE):
             for y in range(Y_SIZE):
                 color = color_to_hex(img.getpixel((x, y)))
-                if color in colors["faces"]:
+                if color == '#000000FF':
+                    img.putpixel((x, y), (0, 0, 0, 255))
+
+                elif color in colors["faces"]:
                     img.putpixel((x, y), color_to_dec(colors["faces"][color][face]))
 
                 elif color in colors["hair"]:
                     img.putpixel((x, y), color_to_dec(colors["hair"][color][hair]))
+
+                elif color in colors["eyes"]:
+                    img.putpixel((x, y), color_to_dec(colors["eyes"][color][eyes]))
 
         return img
